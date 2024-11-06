3679acacf75f542478a4a43f2e0cef217b1ed98c
ovongrider <gnornehc@gmail.com>
2024-11-06 18:23:10 +0800

    feat(reversed).stack逆序使用递归

---
+++ recursive/src/main/java/org/example/stack/ReversedStack.java
@@ -0,0 +1,39 @@
+package org.example.stack;
+
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * <p>文件名称:org.example.stack.ReversedStack</p>
+ * <p>文件描述: </p>
+ * <p>内容摘要: </p>
+ * <p>其他说明: </p>
+ *
+ * @version 1.0
+ * @since 2024/11/6
+ */
+public class ReversedStack {
+
+    private static Integer getAndRemoveLast(Stack<Integer> stack) {
+        Integer result = stack.pop();
+        if (stack.isEmpty()) {
+            return result;
+        } else {
+            Integer last = getAndRemoveLast(stack);
+            stack.push(result);
+            return last;
+        }
+    }
+
+    private static void reversed(Stack<Integer> stack) {
+
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.addAll(List.of(1, 2, 3));
+        System.out.println(stack);
+        System.out.println(getAndRemoveLast(stack));
+        System.out.println(stack);
+    }
+}
