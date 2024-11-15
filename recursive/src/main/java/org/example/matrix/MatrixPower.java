1c1ac3ceae1eb8dbf34147ddbb90b8c92d9002bf
ovongrider <gnornehc@gmail.com>
2024-11-15 14:12:47 +0800

    feat(max_area).最大子矩阵（0，,1）面积

---
+++ recursive/src/main/java/org/example/matrix/MatrixPower.java
@@ -0,0 +1,77 @@
+package org.example.matrix;
+
+/**
+ * <p>文件名称:org.example.matrix.MatrixPower</p>
+ * <p>文件描述: 矩阵快速幂</p>
+ * <p>版权所有: Copyright(C)2019-2022</p>
+ * <p>公司: 趣店集团 </p>
+ * <p>内容摘要: </p>
+ * <p>其他说明: </p>
+ *
+ * @author <a> href="mailto:chenrong@qudian.com">chen</a>
+ * @version 1.0
+ * @since 2024/11/14
+ */
+public class MatrixPower {
+
+    private static long fastPower(long base, long p) {
+        long res = 1;
+        for (; p != 0; p >>=1) {
+            if ((p & 1) != 0) {
+                res *= base;
+            }
+            base *= base;
+        }
+        return res;
+    }
+
+    private static int[][] matrixPower(int[][] m, int p) {
+        int[][] res = new int[m.length][m[0].length];
+        // 构造单位矩阵 E
+        for (int i = 0; i < res.length; i++) {
+            res[i][i] = 1;
+        }
+        int[][] t = m;
+        for (; p != 0; p >>= 1) {
+            if ((p & 1) != 0) {
+                res = produce(res, t);
+            }
+            t = produce(t, t);
+        }
+        return res;
+    }
+
+    private static int[][] produce(int[][] a, int[][] b) {
+        int n = a.length;
+        int m = b[0].length;
+        int k = a[0].length;
+        int[][] ans = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int c = 0; c < k; c++) {
+                    ans[i][j] += a[i][c] * b[c][j];
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static int f(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int[][] base = { { 1, 1 }, { 1, 0 } };
+        int[][] res = matrixPower(base, n - 2);
+        return res[0][0] + res[0][1];
+    }
+
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        long l = f(46);
+        System.out.println(l);
+        System.out.println(System.nanoTime() - start);
+    }
+}
---
+++ recursive/src/main/java/org/example/stack/MaxArea.java
@@ -0,0 +1,63 @@
+package org.example.stack;
+
+import java.util.Stack;
+
+/**
+ * <p>文件名称:org.example.stack.MaxArea</p>
+ * <p>文件描述: 最大子矩阵（0，,1）面积</p>
+ * <p>版权所有: Copyright(C)2019-2022</p>
+ * <p>公司: 趣店集团 </p>
+ * <p>内容摘要: </p>
+ * <p>其他说明: </p>
+ *
+ * @author <a> href="mailto:chenrong@qudian.com">chen</a>
+ * @version 1.0
+ * @since 2024/11/15
+ */
+public class MaxArea {
+    //构建直方图
+    private static int buildMatrix(int[][] arr) {
+        int result = 0;
+        int length = arr[0].length;
+        int[] rectangle = new int[length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < length; j++) {
+                rectangle[j] = 0 == arr[i][j] ? 0 : rectangle[j] + arr[i][j];
+            }
+            result = Math.max(result, maxRectangle(rectangle));
+        }
+        return result;
+    }
+
+    private static int maxRectangle(int[] rectangle) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        for (int i = 0; i < rectangle.length; i++) {
+            while (!stack.isEmpty() && rectangle[stack.peek()] > rectangle[i]) {
+                int j = stack.pop();
+                int k = stack.isEmpty() ? -1 : stack.peek();
+                result = Math.max(result, (i - k - 1) * rectangle[j]);
+            }
+            stack.push(i);
+        }
+        //右边已不存在，更小值
+        while (!stack.isEmpty()) {
+            int j = stack.pop();
+            int k = stack.isEmpty() ? -1 : stack.peek();
+            result = Math.max(result, (rectangle.length - k - 1) * rectangle[j]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 0, 1, 0, 0},
+                {1, 0, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 0, 0, 1, 0}
+        };
+        System.out.println(buildMatrix(arr));
+    }
+}
+
