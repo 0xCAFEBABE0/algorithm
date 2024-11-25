af6839795c876a37507b76d0268488a6aab4ebc0
ovongrider <gnornehc@gmail.com>
2024-11-25 17:46:34 +0800

    feat(linked).回文2

--- recursive/src/main/java/org/example/linked/Palindromic.java
+++ recursive/src/main/java/org/example/linked/Palindromic.java
@@ -5,12 +5,9 @@ import java.util.Stack;
 /**
  * <p>文件名称:org.example.linked.Palindromic</p>
  * <p>文件描述: </p>
- * <p>版权所有: Copyright(C)2019-2022</p>
- * <p>公司: 趣店集团 </p>
  * <p>内容摘要: </p>
  * <p>其他说明: </p>
  *
- * @author <a> href="mailto:chenrong@qudian.com">chen</a>
  * @version 1.0
  * @since 2024/11/25
  */
@@ -42,10 +39,43 @@ public class Palindromic {
         return true;
     }
 
+    /**
+     * 使用半个栈，实现左右对折
+     *
+     * @param head
+     * @return boolean
+     */
+    private static boolean palindromic_2(Node head) {
+        if (null == head.next) {
+            return true;
+        }
+        Stack<Integer> stack = new Stack<>();
+        Node cur = head;
+        Node rightHead = head.next;
+
+        while(null != cur.next && null != cur.next.next) {
+            rightHead = rightHead.next;
+            cur = cur.next.next;
+        }
+
+        while(null != rightHead) {
+            stack.push(rightHead.value);
+            rightHead = rightHead.next;
+        }
+
+        while (!stack.isEmpty()) {
+            if (head.value != stack.pop()) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Node last = new Node(1, null);
         Node head = new Node(1, new Node(2, new Node(3, new Node(2, last))));
 
-        System.out.println(palindromic_1(head));
+        System.out.println(palindromic_2(head));
     }
 }
