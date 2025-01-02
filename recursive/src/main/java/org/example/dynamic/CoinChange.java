0778b7e7020ebd1b4d590fbbc5f33c14eb01666d
ovongrider <gnornehc@gmail.com>
2025-01-02 14:34:49 +0800

    feat(dynamic).换钱数量问题,记忆化搜索

--- recursive/src/main/java/org/example/dynamic/CoinChange.java
+++ recursive/src/main/java/org/example/dynamic/CoinChange.java
@@ -24,10 +24,36 @@ public class CoinChange {
         return res;
     }
 
+    //记忆化搜索
+    private static int p2(int[] arr, int aim) {
+        int[][] cache = new int[arr.length + 1][aim + 1];
+        return process02(arr, 0, aim, cache);
+    }
+
+    private static int process02(int[] arr, int i, int aim, int[][] cache) {
+        int res = 0;
+        if (i == arr.length) {
+            res = 0 == aim ? 1 : 0;
+        } else {
+            int cacheV = 0;
+            for (int j = 0; arr[i] * j <= aim; ++j) {
+                cacheV = cache[i + 1][aim - arr[i] * j];
+                if (0 != cacheV) {
+                    res += -1 == cacheV ? 0 : cacheV;
+                } else {
+                    res += process02(arr, i + 1, aim - arr[i] * j, cache);
+                }
+            }
+        }
+        cache[i][aim] = 0 == res ? -1 : res;
+        return res;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {5, 10, 25, 1};
         int aim = 15;
-        int res = process01(arr, 0, aim);
+        int res = p2(arr, aim);
         System.out.println(res);
     }
 }
